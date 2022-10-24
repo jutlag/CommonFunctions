d10997d4a15582978f53b20f9c7fbee7511bf386
Gurpreet Singh Jutla <37092205+jutlag@users.noreply.github.com>
2022-10-25 10:35:08 +1100

    Update vnetScript.ps1

--- vnetScript.ps1
+++ vnetScript.ps1
@@ -1,10 +1,9 @@
 #action
-Write-Output "Passed arguments:"
-$args
 $null = Update-AzConfig -DisplayBreakingChangeWarning $false
 $ExcludedSubnets = @('GatewaySubnet','AzureFirewallSubnet','AzureFirewallManagementSubnet')
 $virtualNetworks = Get-AzVirtualNetwork
 $subnetsToFix = $virtualNetworks | Get-AzVirtualNetworkSubnetConfig | where {$_.NetworkSecurityGroup -eq $null -and $_.name -notin $ExcludedSubnets}
+Write-host ("Number of subnets to fix {0}" -f $subnetsToFix.count)
 
 foreach($subnetobj in $subnetsToFix){
     $subnetData = ($subnetobj.id).split('/')
@@ -14,12 +13,12 @@ foreach($subnetobj in $subnetsToFix){
     $subnetName = $subnetData[10]
     if($subnetName -notin $ExcludedSubnets){
         $nsgName = ('NSG-{0}' -f $subnetName)
-        #Write-host $resourceGroup  $vnetName $subnetName $nsgName
+        Write-host $resourceGroup  $vnetName $subnetName $nsgName
         $networkSecurityGroupObj = Get-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $resourcegroup -ErrorAction SilentlyContinue
         if(($null -eq $networkSecurityGroupObj) -or ($networkSecurityGroupObj.count -eq 0)){
             $networkSecurityGroupObj = New-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $resourcegroup -Location $vNetObj.Location
         }
-        $null = Set-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vNetObj -NetworkSecurityGroupId $networkSecurityGroupObj.Id -AddressPrefix ($subnetobj.AddressPrefix)
-        $null = $vNetObj | Set-AzVirtualNetwork
+        Set-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vNetObj -NetworkSecurityGroupId $networkSecurityGroupObj.Id -AddressPrefix ($subnetobj.AddressPrefix)
+        $vNetObj | Set-AzVirtualNetwork
     }
 }
