4082466ccbebe7bc6daca12b962363a60a1c3371
Gurpreet Singh <gsjutla@hotmail.com>
2023-09-08 10:53:00 +1000

    Some more comments and fixes

--- Send-EmailwithSendGridRESTAPI.ps1
+++ Send-EmailwithSendGridRESTAPI.ps1
@@ -10,21 +10,24 @@ function Send-EmailwithSendGridRESTAPI {
          
    .Notes
      Author: Gurpreet Singh Jutla 
-     Source: https://lessergeek.com/index.php/2022/09/07/quicktip-sending-email-using-rest-api/SubjectofEmail
+     Source: https://lessergeekwebhosting.azurewebsites.net/2022/09/07/quicktip-sending-email-using-rest-api/ 
 
      Create a new sendgrid account and get the REST API TokenStringFromTwilio. Provide the token to make the REST API Call
 
      This function makes it easy to use the Send Grid API to send email. The code simplifies the REST API call documented
-     on their public documentation. 
+     on their public documentation. This code was first written by me in 2019 as a demonstration during my Free Azure
+     training community session when I saw a student struggling to send email with attachment from his code running in 
+     a virtual machine. I have delibrately removed the attachment piece from this code. You can reach out to me if you 
+     fail to find that process to attach the email. 
          
    .EXAMPLE
          
-   C:\PS>  Send-EmailwithSendGridRESTAPI -Path <'Stringtocheck'> 
+   C:\PS>  Send-EmailwithSendGridRESTAPI -RecieverEmail <'gsjutla@lessergeek.com'> -RecieverDisplayName <'Gurpreet Singh Jutla'>
  #>
   [CmdletBinding()]
   param (
     [Parameter(Mandatory = $True)]
-    [String] $RecieverEmail,
+    [String] $RecieverEmail,  # You can also make this a string of arrays if you want multiple recipients.
     [Parameter(Mandatory = $True)]
     [String] $RecieverDisplayName,
     [Parameter(Mandatory = $True)]
@@ -32,7 +35,7 @@ function Send-EmailwithSendGridRESTAPI {
     [Parameter(Mandatory = $True)]
     [String] $FromDisplayName,
     [Parameter(Mandatory = $True)]
-    [String] $BodyofEmail,
+    [String] $BodyofEmail,  # You can pass the content as a HTML structure
     [Parameter(Mandatory = $True)]
     [String] $TokenStringFromTwilio,
     [Parameter(Mandatory = $True)]
@@ -40,36 +43,36 @@ function Send-EmailwithSendGridRESTAPI {
   )
   Process {
     
-    #Create Header for the REST API call
+    #Create Header for the REST API call.
     $Header = @{
       "authorization" = "Bearer $TokenStringFromTwilio"
     }
     $Body=@{
-      "personalizations" = @(
+      "Personalizations" = @(
                                @{
-                                  "to" = @(
+                                  "To" = @(
                                            @{
                                               "email" = $RecieverEmail
                                               "name" = $RecieverDisplayName
                                             }
                                           )
-                                  "SubjectofEmail" = $SubjectofEmail
+                                  "Subject" = $SubjectofEmail
                                 }
                             )
-        "content" = @(
+        "Content" = @(
                        @{
-                         "type" = "text/html"
-                         "value" = $BodyofEmail
+                         "Type" = "text/html"
+                         "Value" = $BodyofEmail
                         }
                      )
-         "from" = @{
+         "From" = @{
                       "email" = $SenderEmail
                       "name" = $FromDisplayName
                    }
          }
 
-    $BodyJson = $Body | ConvertTo-Json -Depth 4
-
+    # Conver the body hash table value to json structure.
+    $BodyJson = $Body | ConvertTo-Json -Depth 10
 
     #Parameters to Send the mail through Sendgrid
     $Parameters = @{
